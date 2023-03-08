1a2735979b4e910cc28151fde671fea35eb8965d
LilianKanyua <liliankanyua@gmail.com>
2023-03-08 11:50:17 +0300

    first commit

---
+++ assig.js
@@ -0,0 +1,55 @@
+//Question one
+const string1="extravaganza";
+var a="extravaganza"[8];
+console.log(a);
+var b="extravaganza"[9];
+console.log(b);
+var c="extravaganza"[10];
+console.log(c);
+var d="extravaganza"[11];
+console.log(d);
+
+//Question two
+const food=`The quick fox jumped over the lazy dog.`;
+ food[4]="eat"
+console.log(`The quick fox ${"eat"} over the lazy dog.`);
+
+
+
+//Question three
+
+const text4="A wonderful world."
+var split=text4.split(' ');
+console.log(split);
+var x=split[1];
+console.log(x);
+console.log(x.toLocaleUpperCase());
+
+
+
+
+
+
+
+//Question four
+
+const pupils="The pupils are reading in the library";
+var find=pupils.search("are");
+console.log(find);
+const seat="The child was sitting on the table before it fell";
+var loc=seat.search("sitting");
+console.log(loc);
+
+//Question five
+
+const text1="wonderful";
+console.log(text1.toLocaleUpperCase())
+
+const text2="amazing";
+console.log(text2.toLocaleLowerCase())
+ 
+const text3="UndERneath";
+console.log(text3.toLocaleLowerCase())
+
+
+//console.log(seat.match(/sitting/));
